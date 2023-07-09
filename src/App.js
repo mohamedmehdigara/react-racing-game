4a75ccd0d1d4d73c112fa7bd795f6707cdb03f12
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-09 08:16:00 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Game from './components/Game';
 import Results from './components/Results';
 
 const App = () => {
-  const navigate = useNavigate();
-
-  const startGame = () => {
-    navigate('/game');
-  };
-
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home startGame={startGame} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
         <Route path="/results" element={<Results />} />
       </Routes>
